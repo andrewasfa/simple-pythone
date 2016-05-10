40e1ca1ca33c6f6445e229d1a8b5c284f182045e
andrewasfa <andrew.asfaganov@gmail.com>
2016-05-10 16:13:09 +1000

    first upload of code

--- mobilephone.py
+++ mobilephone.py
@@ -1,16 +1,112 @@
+import os, random
+
 class Mobilephone:
 	'Base class for all nodes'
 	mobcount = 0
+	hash
 
-	def __init__(self, mobid, glat, glong):
+	def __init__(self, mobid, glat, glong, ipaddress):
 		self.mobid = mobid
 		self.glat = glat
 		self.glong = glong
+		self.ipaddress = ipaddress
+
 		Mobilephone.mobcount += 1
 
 	def displayCount(self):
 		print("Total mobilephones %d" %Mobilephone.mobcount)
 
 	def displayMobilephone(self):
-		print ("Mobid : ", self.mobid, ", Position : ", self.glat, " ", self.glong)
+		print ("Mobid : ", self.mobid, ", IP: ", self.ipaddress,  ", Position : ", \
+																	 self.glat, " ", self.glong)
+
+	def send(self, ipsrc, ipdest, message):
+		print ("sending to %s", ipdest)
+
+
+	def receive(self, ipsrc, ipdest, q, message):
+		print ("receiving from %s %s", ipsrc, message)
+		nodepath = "testdata" + "/" + str(self.mobid)
+		filename = q + ".txt"
+		if not os.path.exists(nodepath):
+			pass 	#os.makedirs(nodepath) 	
+		else:
+			with open(os.path.join(nodepath,filename), "a") as file:
+				file.write(message)	#appends every option it receives from other nodes
+									# if I'm not interested in this q for now, should I still receive everyone's votes?
+									# if I do, then that is a lot more traffic, but if I don't and I decide to contribute at the end
+									# then I will just need to rely on existing data being passed by other nodes
+
+
+	def receive_q(self, question):
+		nodepath = "testdata" + "/" + str(self.mobid)
+		print ("New question %d", self.mobid)
+		if not os.path.exists(nodepath):
+			os.makedirs(nodepath)
+
+		filename = question + ".txt"
+		with open(os.path.join(nodepath,filename), "w") as file:
+			pass
+	
+	def vote(self, question, numanswers):
+		vote = str(random.SystemRandom().randint(1, numanswers))
+		nodepath = "testdata" + "/" + str(self.mobid)
+		filename = question + ".txt"
+		if not os.path.exists(nodepath):
+			os.makedirs(nodepath)
+
+		with open(os.path.join(nodepath,filename), "w") as file:
+			file.write(vote)
+		self.vote = vote	
+
+	def calculate(self, question, numanswers):
+		nodepath = "testdata" + "/" + str(self.mobid)
+		filename = question + ".txt"
+		if os.path.exists(nodepath):
+			with open(os.path.join(nodepath,filename)) as file:
+				lines = file.readlines()
+				results = {}
+				print ("lines", lines)
+				for char in lines[0]: 
+					print ("char", char)
+					print results
+					if char in results:
+						results[char] += 1
+						print str(char) + "inc here"
+					else:
+						results[char] = 1
+						print str(char) + "new here"
+			# now print results
+			for key, count in results.iteritems():
+				print "Key, count", key, count
+
+
+
+
+class Carrier:
+	'Base class for all connecting carriers'
+
+	def __init__(self, bizname):
+		self.bizname = bizname
+		dict = {'user1': '10.0.0.2', 'user2': '10.0.0.3', 'user3': '10.0.0.4'}
+
+	def sendTo(self, ipsrc, ipdest):
+		print ("Carrier sending to %d", ipdest)
+
+
+
+
+	def find(self, ipdest):
+		pass
+
+
+'''
+Public methods:
+	- sendTo
+
+Internal methods:
+	- findIP
+
+
+'''
 
--- simdriver.py
+++ simdriver.py
@@ -1,14 +1,103 @@
 from mobilephone import Mobilephone
-
-'Create the nodes'
-mob1 = Mobilephone(1, 1, 1)
-mob2 = Mobilephone(2, 2, 2)
-mob3 = Mobilephone(3, 3, 3)
-
+from mobilephone import Carrier
+import argparse 
+from time import sleep
+import string, random
 
 
-'Display the nodes'
-mob1.displayMobilephone()
-mob2.displayMobilephone()
-mob3.displayMobilephone()
-print("Total number of nodes: %d" % Mobilephone.mobcount)
+parser = argparse.ArgumentParser(description="Representer for the network, usage simdiver 10")
+parser.add_argument("numnodes", type=int)
+parser.add_argument("qoptions", type=int)
+args = parser.parse_args()
+
+nodes_list = []	
+for i in nodes_list:
+	del i 
+
+def create_nodes(numnodes):
+	'Create the nodes and add it into the list'
+
+
+	for i in range(numnodes):
+		
+		# First we assign the mobid, if it already exists, we keep generating a new one until its
+		# unique. This is not used currently, because in testing we can just assign them sequentially
+		''' mobid = random.SystemRandom().randint(1, numnodes)
+		 itemnumber = 0
+		 while itemnumber < len(nodes_list):
+			if (nodes_list[itemnumber].mobid == mobid):
+				print "caught! " + str(nodes_list[itemnumber].mobid) + " " + str(mobid)
+				mobid = random.SystemRandom().randint(1, numnodes)
+				itemnumber = 0
+				print "new " + str(mobid) +" resetting"
+			else:
+				itemnumber +=1 
+		'''
+		mobid = i +1		
+		# At this stage (in the early version) the location attributesare not important
+		glat = "glat"
+		glong = "glang"
+		ipaddress = "10.0.0." + (str(random.SystemRandom().randint(0, 255)))
+		phone_obj = Mobilephone(mobid, glat, glong, ipaddress)
+		nodes_list.append(phone_obj)
+		
+		#for n in nodes_list:
+		#	print n.mobid
+		#rand = random.SystemRandom()
+		#lastOctet = str(rand.randint(0, 255))
+		
+
+
+
+def generate_question():
+
+	q = "q1"
+	n = args.qoptions
+	for i in nodes_list:
+		i.receive_q(q)
+	return (q,n) 
+
+def generate_responses(q, options):
+
+	for n in nodes_list:
+		n.vote(q, options)
+		print "generated for "+ str(n.mobid)
+
+def broadcast(q):
+
+	for n in nodes_list:		# get every node to broadcast their vote
+		for dest in nodes_list:	# iterate through every node to receive others' votes
+			if n != dest:		# 
+				print dest.vote
+				n.receive(n.ipaddress, dest.ipaddress, q, dest.vote)
+
+def get_results(q, options):
+	for n in nodes_list:
+		n.calculate(q, options)
+
+
+
+if __name__ == "__main__":
+	
+	create_nodes(args.numnodes)
+
+	'Print the nodes'
+	for i in nodes_list:
+		print "phone", i.displayMobilephone()
+
+	print("Total number of nodes: %d" % Mobilephone.mobcount)
+
+	carrier1 = Carrier("Telstra")
+	tupq = generate_question()
+	
+	question =tupq[0]
+	numanswers = tupq[1]
+	options = int(numanswers)
+
+	generate_responses(question, options)
+	broadcast(question)	
+	get_results(question, numanswers)
+
+
+#	mob1.send(mob1.ipaddress, mob3.ipaddress)
+#	carrier1.sendTo(mob1.ipaddress, mob3.ipaddress)
